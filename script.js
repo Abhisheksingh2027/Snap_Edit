f3e48b2f52c860c79cdb983a1aa9089f357ffe76
Abhisheksingh2027 <abhisingh.nimiyadih@gmail.com>
2026-04-20 00:20:23 +0530

    work on right side part

---
+++ script.js
@@ -0,0 +1,92 @@
+const filters ={
+    brightness:{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    contras:{
+        value:100,
+        min:0,
+        max:200,
+         unit:"%"
+
+    },
+    exposer : {
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    saturation :{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    hueRotation :{
+        value:0,
+        min:0,
+        max:360,
+        unit:"deg"
+    },
+    blur:{
+        value:0,
+        min:0,
+        max:20,
+        unit:"px"
+    },
+    grayscale :{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    sepia :{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    opacity:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+
+    invert:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },        
+
+}
+
+
+function createFilterElement(name, unit="%",value,min,max){
+    const div = document.createElement("div");
+    div.classList.add("filters");
+
+    const input= document.createElement("input");
+    input.type="range";
+    input.min=min;
+    input.max= max;
+    input.value=value;
+    input.id= name;
+
+    const p= document.createElement("p");
+    p.innerText= name;
+
+    div.appendChild(p);
+    div.appendChild(input);
+
+    return div;
+
+
+}
+
+Object.keys(filters).forEach(key =>{
+    console.log(key);
+})
